9cdf1a723271f0b406a897a41b87362710afa000
Jonas Westman <jonaswestman@hotmail.com>
2024-02-18 10:44:23 +0100

    physics

--- src/main.c
+++ src/main.c
@@ -8,8 +8,7 @@
 #include "engine/config.h"
 #include "engine/input.h"
 #include "engine/time.h"
-
-#define _CRT_SECURE_NO_WARNINGS
+#include "engine/physics.h"
 
 static bool should_quit = false;
 static vec2 pos;
@@ -31,12 +30,25 @@ int main(int argc, char *argv[]) {
     time_init(60);
     config_init();
     render_init();
+    physics_init();
+
+    u32 body_count = 100;
+
+    for (u32 i = 0; i < body_count; ++i) {
+        usize body_index = physics_body_create(
+            (vec2){ rand() % (i32)global.render.width, rand() % (i32)global.render.height },
+            (vec2){ rand() % 100, rand() % 100 });
+            Body *body = physics_body_get(body_index);
+            body->acceleration[0] = rand() % 200 - 100;
+            body->acceleration[1] = rand() % 200 - 100;
+    }
 
     pos[0] = global.render.width * 0.5;
     pos[1] = global.render.height * 0.5;
 
     while (!should_quit) {
         time_update();
+
         SDL_Event event;
 
         while (SDL_PollEvent(&event)) {
@@ -53,10 +65,30 @@ int main(int argc, char *argv[]) {
 
         input_update();
         input_handle();
+        physics_update();
 
         render_begin();
 
         render_quad(pos, (vec2){50, 50}, (vec4){0, 1, 0, 1});
+
+        for (u32 i = 0; i < body_count; ++i) {
+			Body *body = physics_body_get(i);
+			render_quad(body->aabb.position, body->aabb.half_size, (vec4){1, 0, 0, 1});
+
+			if (body->aabb.position[0] > global.render.width || body->aabb.position[0] < 0)
+				body->velocity[0] *= -1;
+			if (body->aabb.position[1] > global.render.height || body->aabb.position[1] < 0)
+				body->velocity[1] *= -1;
+
+			if (body->velocity[0] > 500)
+				body->velocity[0] = 500;
+			if (body->velocity[0] < -500)
+				body->velocity[0] = -500;
+			if (body->velocity[1] > 500)
+				body->velocity[1] = 500;
+			if (body->velocity[1] < -500)
+				body->velocity[1] = -500;
+		}
 
         render_end(); 
         time_update_late();
