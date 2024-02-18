27cf64854eddadf9fb7d1c447f7c73ce6d40baf4
Jonas Westman <jonaswestman@hotmail.com>
2024-02-18 11:43:05 +0100

    point abb intersection

--- src/engine/physics/physics.c
+++ src/engine/physics/physics.c
@@ -6,7 +6,20 @@
 #include "physics_internal.h"
 
 static Physics_State_Internal state;
-int timeframe = 0;
+
+void aabb_min_max(vec2 min, vec2 max, AABB aabb) {
+	vec2_sub(min, aabb.position, aabb.half_size);
+	vec2_add(max, aabb.position, aabb.half_size);
+}
+
+void physics_point_intersect_aabb(vec2 point, AABB aabb) {
+	vec2 min, max;
+	aabb_min_max(min, max, aabb);
+	return  point[0] >= min[0] &&
+			point[0] <= max[0] &&
+			point[1] >= min[1] &&
+			point[1] <= max[1];
+}
 
 void physics_init(void)
 {
@@ -20,17 +33,12 @@ void physics_update(void)
 	for (u32 i = 0; i < state.body_list->len; ++i) {
 		body = array_list_get(state.body_list, i);
 
-		if (timeframe <= 0)
-			printf("Index: %i, Vel x: %f, Vel y: %f \n", i, body->acceleration[0], body->acceleration[1]);
-
 		body->velocity[0] += body->acceleration[0] * global.time.delta;
 		body->velocity[1] += body->acceleration[1] * global.time.delta;
 		body->aabb.position[0] += body->velocity[0] * global.time.delta;
 		body->aabb.position[1] += body->velocity[1] * global.time.delta;
 
 	}
-
-	timeframe = 1;
 }
 
 usize physics_body_create(vec2 position, vec2 size)
--- src/main.c
+++ src/main.c
@@ -1,105 +1,77 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
-#include <glad\glad.h>
+#include <glad/glad.h>
 #define SDL_MAIN_HANDLED
-#include <SDL2\SDL.h>
+#include <SDL2/SDL.h>
 
 #include "engine/global.h"
 #include "engine/config.h"
 #include "engine/input.h"
 #include "engine/time.h"
 #include "engine/physics.h"
+#include "engine/util.h"
 
 static bool should_quit = false;
 static vec2 pos;
 
 static void input_handle(void) {
-    if (global.input.left == KS_PRESSED || global.input.left == KS_HELD)
-        pos[0] -= 500 * global.time.delta;
-    if (global.input.right == KS_PRESSED || global.input.right == KS_HELD)
-        pos[0] += 500 * global.time.delta;
-    if (global.input.up == KS_PRESSED || global.input.up == KS_HELD)
-        pos[1] += 500 * global.time.delta;
-    if (global.input.down == KS_PRESSED || global.input.down == KS_HELD)
-        pos[1] -= 500 * global.time.delta;
-    if (global.input.escape == KS_PRESSED || global.input.escape == KS_HELD)
-        should_quit = true;
+	if (global.input.escape == KS_PRESSED || global.input.escape == KS_HELD)
+		should_quit = true;
+
+	i32 x, y;
+	SDL_GetMouseState(&x, &y);
+	pos[0] = (f32)x;
+	pos[1] = global.render.height - y;
 }
 
 int main(int argc, char *argv[]) {
-    time_init(60);
-    config_init();
-    render_init();
-    physics_init();
+	time_init(60);
+	config_init();
+	render_init();
+	physics_init();
 
-    u32 body_count = 100;
+	pos[0] = global.render.width * 0.5;
+	pos[1] = global.render.height * 0.5;
 
-    printf("Acc init ======================================\n");
-    for (u32 i = 0; i < body_count; ++i) {
-        usize body_index = physics_body_create(
-            (vec2){ rand() % (i32)global.render.width, rand() % (i32)global.render.height },
-            (vec2){ rand() % 100, rand() % 100 });
-            Body *body = physics_body_get(body_index);
-            body->acceleration[0] = rand() % 200 - 100;
-            body->acceleration[1] = rand() % 200 - 100;
-            printf("Index : %i, Acc x: %f, Acc y: %f \n", i, body->acceleration[0], body->acceleration[1]);
-    }
-    printf("Acc init ======================================\n");
+	SDL_ShowCursor(false);
 
-    pos[0] = global.render.width * 0.5;
-    pos[1] = global.render.height * 0.5;
-    
-    while (!should_quit) {
-        time_update();
+	AABB test_aabb = {
+		.position = {global.render.width * 0.5, global.render.height * 0.5},
+		.half_size = {50, 50}
+	};
 
-        SDL_Event event;
+	while (!should_quit) {
+		time_update();
 
-        while (SDL_PollEvent(&event)) {
-            switch (event.type)
-            {
-            case SDL_QUIT:
-                should_quit = true;
-                break;
-            
-            default:
-                break;
-            }
-        }
+		SDL_Event event;
 
-        input_update();
-        input_handle();
-        physics_update();
-
-        render_begin();
-
-        render_quad(pos, (vec2){50, 50}, (vec4){0, 1, 0, 1});
-
-        for (u32 i = 0; i < body_count; ++i)
-        {
-            Body* body = physics_body_get(i);
-
-			render_quad(body->aabb.position, body->aabb.half_size, (vec4){1, 0, 0, 1});
-
-			if (body->aabb.position[0] > global.render.width || body->aabb.position[0] < 0)
-				body->velocity[0] *= -1;
-			if (body->aabb.position[1] > global.render.height || body->aabb.position[1] < 0)
-				body->velocity[1] *= -1;
-
-			if (body->velocity[0] > 500)
-				body->velocity[0] = 500;
-			if (body->velocity[0] < -500)
-				body->velocity[0] = -500;
-			if (body->velocity[1] > 500)
-				body->velocity[1] = 500;
-			if (body->velocity[1] < -500)
-				body->velocity[1] = -500;
-
-            printf("Index : %i, Acc x: %f, Acc y: %f \n", i, body->acceleration[0], body->acceleration[1]);
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+			case SDL_QUIT:
+				should_quit = true;
+				break;
+			default:
+				break;
+			}
 		}
 
-        render_end(); 
-        time_update_late();
-    }
+		input_update();
+		input_handle();
+		physics_update();
 
-    return 0;
+		render_begin();
+
+		render_aabb((f32*)&test_aabb, (vec4){1, 1, 1, 0.5});
+
+		if (physics_point_intersect_aabb(pos, test_aabb))
+			render_quad(pos, (vec2){5, 5}, RED);
+		else
+			render_quad(pos, (vec2){5, 5}, WHITE);
+
+		render_end();
+		time_update_late();
+	}
+
+	return 0;
 }
