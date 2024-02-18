ac830868f10c944f3ea698497df37a92266f212d
Jonas Westman <jonaswestman@hotmail.com>
2024-02-18 18:42:49 +0100

    collision response and collision layers

---
+++ src/engine/ui.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <linmath.h>
+
+typedef struct panel {
+    vec2 position;
+    vec2 dimensions;
+    vec4 color;
+} Panel;
--- src/main.c
+++ src/main.c
@@ -11,18 +11,68 @@
 #include "engine/time.h"
 #include "engine/physics.h"
 #include "engine/util.h"
+#include "engine/entity.h"
+#include "engine/ui.h"
+
+typedef enum collision_layer {
+	COLLISION_LAYER_PLAYER = 1,
+	COLLISION_LAYER_ENEMY = 1 << 1,
+	COLLISION_LAYER_TERRAIN = 1 << 2,
+} Collision_Layer;
 
 static bool should_quit = false;
-static vec2 pos;
+vec4 player_color = {0, 1, 1, 1};
+bool player_is_grounded = false;
 
-static void input_handle(void) {
-	if (global.input.escape == KS_PRESSED || global.input.escape == KS_HELD)
+static void input_handle(Body *body_player) {
+	if (global.input.escape)
 		should_quit = true;
 
-	i32 x, y;
-	SDL_GetMouseState(&x, &y);
-	pos[0] = (f32)x;
-	pos[1] = global.render.height - y;
+	f32 velx = 0;
+	f32 vely = body_player->velocity[1];
+
+	if (global.input.right) {
+		velx += 1000;
+	}
+
+	if (global.input.left) {
+		velx -= 1000;
+	}
+	
+	if (global.input.up && player_is_grounded) {
+		player_is_grounded = false;
+		vely = 4000;
+	}
+
+	if (global.input.down) {
+		vely -= 800;
+	}
+
+	body_player->velocity[0] = velx;
+	body_player->velocity[1] = vely;
+}
+
+void player_on_hit(Body *self, Body *other, Hit hit) {
+	if (other->collision_layer == COLLISION_LAYER_ENEMY) {
+		player_color[0] = 1;
+		player_color[2] = 0;
+	}
+}
+
+void player_on_hit_static(Body *self, Static_Body *other, Hit hit) {
+	if (hit.normal[1] > 0) {
+		player_is_grounded = true;
+	}
+}
+
+void enemy_on_hit_static(Body *self, Static_Body *other, Hit hit) {
+	if (hit.normal[0] > 0) {
+		self->velocity[0] = 700;
+	}
+
+	if (hit.normal[0] < 0) {
+		self->velocity[0] = -700;
+	}
 }
 
 int main(int argc, char *argv[]) {
@@ -30,16 +80,26 @@ int main(int argc, char *argv[]) {
 	config_init();
 	render_init();
 	physics_init();
-
-	pos[0] = global.render.width * 0.5;
-	pos[1] = global.render.height * 0.5;
+	entity_init();
 
 	SDL_ShowCursor(false);
 
-	AABB test_aabb = {
-		.position = {global.render.width * 0.5, global.render.height * 0.5},
-		.half_size = {50, 50}
-	};
+	u8 enemy_mask = COLLISION_LAYER_PLAYER | COLLISION_LAYER_TERRAIN;
+	u8 player_mask = COLLISION_LAYER_ENEMY | COLLISION_LAYER_TERRAIN;
+
+	usize player_id = entity_create((vec2){100, 800}, (vec2){100, 100}, (vec2){0, 0}, COLLISION_LAYER_PLAYER, player_mask, player_on_hit, player_on_hit_static);
+
+	f32 width = global.render.width;
+	f32 height = global.render.height;
+
+	u32 static_body_a_id = physics_static_body_create((vec2){width * 0.5 - 25, height - 25}, (vec2){width - 50, 50}, COLLISION_LAYER_TERRAIN);
+	u32 static_body_b_id = physics_static_body_create((vec2){width - 25, height * 0.5 + 25}, (vec2){50, height - 50}, COLLISION_LAYER_TERRAIN);
+	u32 static_body_c_id = physics_static_body_create((vec2){width * 0.5 + 25, 25}, (vec2){width - 50, 50}, COLLISION_LAYER_TERRAIN);
+	u32 static_body_d_id = physics_static_body_create((vec2){25, height * 0.5 - 25}, (vec2){50, height - 50}, COLLISION_LAYER_TERRAIN);
+	u32 static_body_e_id = physics_static_body_create((vec2){width * 0.5, height * 0.5}, (vec2){150, 150}, COLLISION_LAYER_TERRAIN);
+
+	usize entity_a_id = entity_create((vec2){600, 600}, (vec2){50, 50}, (vec2){900, 0}, COLLISION_LAYER_ENEMY, enemy_mask, NULL, enemy_on_hit_static);
+	usize entity_b_id = entity_create((vec2){800, 800}, (vec2){50, 50}, (vec2){900, 0}, 0, enemy_mask, NULL, enemy_on_hit_static);
 
 	while (!should_quit) {
 		time_update();
@@ -56,20 +116,35 @@ int main(int argc, char *argv[]) {
 			}
 		}
 
+		Entity *player = entity_get(player_id);
+		Body *body_player = physics_body_get(player->body_id);
+		Static_Body *static_body_a = physics_static_body_get(static_body_a_id);
+		Static_Body *static_body_b = physics_static_body_get(static_body_b_id);
+		Static_Body *static_body_c = physics_static_body_get(static_body_c_id);
+		Static_Body *static_body_d = physics_static_body_get(static_body_d_id);
+		Static_Body *static_body_e = physics_static_body_get(static_body_e_id);
+
 		input_update();
-		input_handle();
+		input_handle(body_player);
 		physics_update();
 
 		render_begin();
 
-		render_aabb((f32*)&test_aabb, (vec4){1, 1, 1, 0.5});
+		render_aabb((f32*)static_body_a, WHITE);
+		render_aabb((f32*)static_body_b, WHITE);
+		render_aabb((f32*)static_body_c, WHITE);
+		render_aabb((f32*)static_body_d, WHITE);
+		render_aabb((f32*)static_body_e, WHITE);
+		render_aabb((f32*)body_player, player_color);
 
-		if (physics_point_intersect_aabb(pos, test_aabb))
-			render_quad(pos, (vec2){5, 5}, RED);
-		else
-			render_quad(pos, (vec2){5, 5}, WHITE);
+		render_aabb((f32*)physics_body_get(entity_get(entity_a_id)->body_id), WHITE);
+		render_aabb((f32*)physics_body_get(entity_get(entity_b_id)->body_id), WHITE);
 
 		render_end();
+
+		player_color[0] = 0;
+		player_color[2] = 1;
+		
 		time_update_late();
 	}
 
